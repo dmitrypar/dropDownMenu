6ab1a6fa999504afc086bcb0a1ab4f420b4bbc5f
dmitriyPC <dmitrypn81@gmail.com>
2020-09-25 16:12:58 +0300

    Updates

---
+++ precache-manifest.2827867b6ac8c7a53369942aee939f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba32fd57112d5873949bff9d2024c92",
+    "url": "/dropDownMenu/index.html"
+  },
+  {
+    "revision": "1a8a7938a1b5ac40b5f5",
+    "url": "/dropDownMenu/static/css/main.7b3f20ee.chunk.css"
+  },
+  {
+    "revision": "5c04692a26f16d646b1b",
+    "url": "/dropDownMenu/static/js/2.ab30f356.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dropDownMenu/static/js/2.ab30f356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a8a7938a1b5ac40b5f5",
+    "url": "/dropDownMenu/static/js/main.ba96d236.chunk.js"
+  },
+  {
+    "revision": "e5af86f376b36ba96582",
+    "url": "/dropDownMenu/static/js/runtime-main.01c4fc6d.js"
+  }
+]);
